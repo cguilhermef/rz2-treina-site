19c93bcedcfe42fa648b91877872a261e0264979
Christian G Fritsch <christian@RZ-MC001.local>
2018-08-29 18:01:04 -0300

    Implementado para que o primeiro box dos blocos 2 e 3 seja aberto quando o bloco é acessado

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
   Navigation();
-  BoardTips('board-1');
-  BoardTips('board-2');
+  let timer = null;
+  const board1 = BoardTips('board-1').init();
+  const board2 = BoardTips('board-2').init();
+  $(document).on('click', '.js-content', function(event) {
+    timer = clearTimeout(timer);
+    timer = setTimeout(function() {
+      board1.activeContent($(event.currentTarget).data('content'));
+    }, 800);
+  });
+  $(document).on('click', '.js-content-space', function(event) {
+    timer = clearTimeout(timer);
+    timer = setTimeout(function() {
+      board2.activeContent($(event.currentTarget).data('content'));
+    }, 800);
+  });
 });
 
 var Navigation = function() {
@@ -138,51 +151,61 @@ var BoardTips = function(boardId) {
   board$.prepend(drawBox$);
   const svg1 = SVG(`js-draw-${ boardId }`).size(board$.outerWidth(), board$.outerHeight());
   let running = false;
-  board$.on('click', '.js-module', function(event) {
-    if (running) {
-      return;
-    }
-    running = true;
-    const el$ = $(event.currentTarget);
-    const contentTarget = el$.data('content');
-    const content$ = $(`#${ contentTarget}`);
+  return {
+    activeContent: function(contentId) {
+      if (running) {
+        return;
+      }
+      const el$ = board$.find(`.js-module[data-content="${ contentId }"]`);
+      if ( el$.hasClass('--active')) {
+        return;
+      }
+      running = true;
+      board$.find('.js-module').removeClass('--active'); 
+      
+      el$.addClass('--active');
+      const content$ = $(`#${ contentId}`);
     
-    svg1.clear();
-    
-    board$.find('.js-module').removeClass('--active');
-    contents$.hide();
-    content$.fadeIn();
-      // 
-    let x1;
-    if (window.screen.outerWidth < 992) {
-      x1 = content$.offset().left + (( content$.outerWidth() / 2) * .3);
-    } else {
-      x1 = content$.offset().left + ( content$.outerWidth() / 2);
+      svg1.clear();
+      contents$.hide();
+      content$.fadeIn();
+      let x1;
+      if (window.screen.outerWidth < 992) {
+        x1 = content$.offset().left + (( content$.outerWidth() / 2) * .3);
+      } else {
+        x1 = content$.offset().left + ( content$.outerWidth() / 2);
+      }
+      let x2 = el$.offset().left + (el$.outerWidth() / 2) - 6;// - el$.outerWidth()*.6;
+      let y1 = content$.offset().top + content$.outerHeight();
+      let y2 = el$.offset().top - 6; // + el$.outerHeight() * .2;
+      if ( y2 < y1 ) {
+        y1 = content$.offset().top - 6;
+      }
+      let line1 = svg1.line(x1, y1, x1, y1);
+      const circle1 = svg1.circle(12).fill('#fff').stroke({color: '#ff9500', width: 3}).move(x1 - 6, y1 - 6);
+      line1
+        .animate(200)
+        .plot(x1, y1, x1, y2)
+        .after(function() {
+          const line2 = svg1.line(x1, y2, x1, y2);
+          line2.stroke({ color: '#ff9500', width: 4, linecap: 'round'});
+          line2
+            .animate(200)
+            .plot(x1, y2, x2, y2)
+            .after(function() {
+              const circle2 = svg1.circle(12).fill('#fff').stroke({color: '#ff9500', width: 3}).move(x2, y2 - 6 );
+              running = false;
+            });
+        })
+      line1.stroke({ color: '#ff9500', width: 4, linecap: 'round'});
+    },
+    init: function() {
+      const self = this;
+      board$.on('click', '.js-module', function(event) {
+        console.log($(event.currentTarget));
+        self.activeContent($(event.currentTarget).data('content'));
+      });
+      return self;
     }
-    let x2 = el$.offset().left + (el$.outerWidth() / 2) - 6;// - el$.outerWidth()*.6;
-    let y1 = content$.offset().top + content$.outerHeight();
-    let y2 = el$.offset().top - 6; // + el$.outerHeight() * .2;
-    if ( y2 < y1 ) {
-      y1 = content$.offset().top - 6;
-    }
-    let line1 = svg1.line(x1, y1, x1, y1);
-    const circle1 = svg1.circle(12).fill('#fff').stroke({color: '#ff9500', width: 3}).move(x1 - 6, y1 - 6);
-    line1
-      .animate(200)
-      .plot(x1, y1, x1, y2)
-      .after(function() {
-        const line2 = svg1.line(x1, y2, x1, y2);
-        line2.stroke({ color: '#ff9500', width: 4, linecap: 'round'});
-        line2
-          .animate(200)
-          .plot(x1, y2, x2, y2)
-          .after(function() {
-            const circle2 = svg1.circle(12).fill('#fff').stroke({color: '#ff9500', width: 3}).move(x2, y2 - 6 );
-            running = false;
-          });
-      })
-    line1.stroke({ color: '#ff9500', width: 4, linecap: 'round'});
-    el$.addClass('--active');
-    
-  });
+  }
 }
